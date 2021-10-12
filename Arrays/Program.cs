92dc804ea030832e2f78c738a826af1d710310fb
Spener-Chapman <spencerchap@hotmail.com>
2021-10-12 09:37:41 -0300

    update 1
    before arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,41 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("ammo system");
+            // we wish to store the ammo of all weapons:
+            //pistol 6/6
+            // shotgun 2/2
+            // ... etc
+
+            int weapon = 0; // 0 = pistol, 1 = shotgun, ... etc
+
+            
+            Console.WriteLine("BEFORE arrays:");
+
+            int pistolAmmo = 6; // declaration & initialization
+            int shotGunAmmo = 2;
+            // .... etc
+
+            // how do we show the ammo of teh current weapon?
+            if (weapon == 0)
+            {
+                Console.WriteLine("pistol ammo:" + pistolAmmo); 
+            }
+            else if (weapon == 1)
+            {
+                Console.WriteLine("shotgun ammo:" + shotGunAmmo);
+            }
+            // etc
+
+
+            Console.WriteLine("arrays:");
+
+
+
+
+
+
+            Console.ReadKey(true);
         }
     }
 }
